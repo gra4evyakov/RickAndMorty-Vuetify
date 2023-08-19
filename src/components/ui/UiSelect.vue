5749ffb723ec04f264e83d81e9de67add441c50e
gra4evyakov <gra4evyakov@yandex.ru>
2023-08-19 10:09:10 +0300

    add ui-select

---
+++ src/components/ui/UiSelect.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useCharactersStore } from '@/store/app';
+
+const store = useCharactersStore()
+</script>
+
+<template>
+    <v-row class="justify-center">
+        <v-col sm="6">
+            <v-select
+                v-model="store.fetchQuery.species"
+                density="compact"
+                label="Select"
+                :items="store.filters"
+                variant="solo-filled"
+            />
+        </v-col>
+    </v-row>
+</template>
